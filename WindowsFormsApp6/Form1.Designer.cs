8bbaaedb300e602a671b1b1f0abeeb9b49752ee0
Алексей Закалюжный <aleksey.zakaluzniy@gmail.com>
2019-11-06 21:31:17 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(1184, 602);
+            this.splitContainer1.SplitterDistance = 563;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(534, 587);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.TreeViev1_BeforeExpand);
+            this.treeView1.BeforeSelect += new System.Windows.Forms.TreeViewCancelEventHandler(this.TreeView1_BeforeSelect);
+            this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(6, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(611, 587);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1184, 602);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            treeView1.BeforeSelect += TreeView1_BeforeSelect;
+            treeView1.BeforeExpand += TreeViev1_BeforeExpand;
+            FillDriveNodes();
+            var currentDirectory = System.IO.Directory.GetCurrentDirectory();
+            ImageList imageList1 = new ImageList();
+            imageList1.Images.Add(Bitmap.FromFile(currentDirectory+@"\file.jpg"));
+            listView1.SmallImageList = imageList1;
+            ImageList imageList2 = new ImageList();
+            imageList2.Images.Add(Bitmap.FromFile(currentDirectory +@"\folder.jpg"));
+            treeView1.ImageList = imageList2;
+        }
+
+        /// <summary>
+        /// Получение дочерних узлов для определенного узла
+        /// </summary>
+        /// <param name="node">Узел, для которого хотим получить</param>
+        /// <param name="path">Путь к узлу</param>
+        private void FillTreeNode (TreeNode node, string path)
+        {
+            try
+            {
+                string[] dirs = Directory.GetDirectories(path);
+                foreach (var directory in dirs)
+                {
+                    var Node = new TreeNode();
+                    Node.Text = directory.Remove(0, directory.LastIndexOf("\\") + 1);
+                    node.Nodes.Add(Node);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+
+        /// <summary>
+        /// Получение всех дисков на ПК
+        /// </summary>
+        private void FillDriveNodes()
+        {
+            try
+            {
+                foreach (DriveInfo drive in DriveInfo.GetDrives())
+                {
+                    var Node = new TreeNode(Text = drive.Name) ;
+                    Node.Tag = drive;
+                    FillTreeNode(Node, drive.Name);
+                    treeView1.Nodes.Add(Node);
+                }
+            }
+            catch
+            {
+            }
+        }
+
+
+        // Событие перед раскрытием узла
+        void TreeViev1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            e.Node.Nodes.Clear();
+            string[] dirs;
+            try
+            {
+                dirs = Directory.GetDirectories(e.Node.FullPath);
+                if (dirs.Length != 0)
+                {
+                    for (int i = 0; i < dirs.Length; i++)
+                    {
+                      DirectoryInfo info = new DirectoryInfo(dirs[i]);
+                      var Node = new TreeNode(info.Name);
+                      Node.Tag = info;
+                      FillTreeNode(Node, dirs[i]);
+                      e.Node.Nodes.Add(Node);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        //Событие перед выделением узла
+        void TreeView1_BeforeSelect (object sender, TreeViewCancelEventArgs e)
+        {
+            e.Node.Nodes.Clear();
+            string[] dirs;
+            if (Directory.Exists(e.Node.FullPath))
+            {
+                dirs = Directory.GetDirectories(e.Node.FullPath);
+                if (dirs.Length != 0)
+                {
+                    for (int i = 0; i < dirs.Length; i++)
+                    {
+                        DirectoryInfo info = new DirectoryInfo(dirs[i]);
+                        var Node = new TreeNode(info.Name);
+                        Node.Tag = info;
+                        FillTreeNode(Node, dirs[i]);
+                        e.Node.Nodes.Add(Node);
+                    }
+                }
+            }
+        }
+
+        //Событие, при проишествии которого открывается содержимое папки (двойной клик)
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            listView1.Items.Clear();
+            var nodeDirInfo = (DirectoryInfo)e.Node.Tag;
+            ListViewItem.ListViewSubItem[] subItems;
+            ListViewItem item = null;
+            foreach (FileInfo file in nodeDirInfo.GetFiles())
+            {
+                item = new ListViewItem(file.Name + " " + file.Length + " bytes", 0);
+                subItems = new ListViewItem.ListViewSubItem[]
+                {
+                    new ListViewItem.ListViewSubItem(item, "File"),
+                    new ListViewItem.ListViewSubItem(item,file.LastAccessTime.ToShortDateString())
+                };
+                item.SubItems.AddRange(subItems);
+                listView1.Items.Add(item);
+            }
+            listView1.Sorting = SortOrder.Ascending;
+        }
+
+    }
+}
